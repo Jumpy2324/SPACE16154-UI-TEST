8db905e41265f16401283fa80f5837ee86ef2d9c
Jumpy2324 <gianmarco.candeletti@hotmail.it>
2026-08-22 00:16:17 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("install",e=>self.skipWaiting());
-self.addEventListener("activate",e=>e.waitUntil((async()=>{
-  const keys=await caches.keys();
-  await Promise.all(keys.filter(k=>/space16154/i.test(k)).map(k=>caches.delete(k)));
-  await self.registration.unregister();
-  const cs=await self.clients.matchAll({type:"window"});
-  cs.forEach(c=>c.navigate(c.url));
-})()));
-self.addEventListener("fetch",()=>{});
